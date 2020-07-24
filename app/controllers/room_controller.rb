8008ddd47deaacb546b039a75d0321246d097b09
taka.s <takapie81@gmail.com>
2020-07-24 17:17:45 +0000

    add model post

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,6 +1,7 @@
 class RoomController < ApplicationController
   def show
     @room = Room.find(params[:id])
+    @posts = @room.posts
   end
   
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_165103) do
+ActiveRecord::Schema.define(version: 2020_07_24_171159) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
